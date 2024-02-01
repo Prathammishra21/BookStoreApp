c4f079295da6ca0efa87a01ca7165f4b4e84fa38
Pratham Mishra <mishrapratham119@gmail.com>
2024-02-01 13:19:04 +0530

    controller and service details

--- bookstore/src/main/java/com/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/bookstore/controller/BookController.java
@@ -1,22 +1,49 @@
 package com.bookstore.controller;
 
+import com.bookstore.entity.Book;
+import com.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
 
 @Controller
 public class BookController {
-
+    @Autowired
+    private BookService service;
     @GetMapping("/")
     public String home(){
         return "home";
     }
+
+
     @GetMapping("/book_register")
     public String bookRegister(){
         return "bookRegister";
     }
+
+
     @GetMapping("/available_books")
-    public String getAllBook(){
-        return "booklist";
+    public ModelAndView getAllBook() {
+        List<Book> list = service.getAllBook();
+        return new ModelAndView("bookList", "book", list);
     }
 
-}
+        @PostMapping("/save")
+                public String addBook(@ModelAttribute Book b){
+            service.save(b);
+            return "redirect:/available_books";
+        }
+
+        @GetMapping("my_Books")
+    public String getMyBooks(){
+        return "myBooks";
+        }
+    }
+
+
