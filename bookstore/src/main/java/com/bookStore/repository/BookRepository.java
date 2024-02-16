68b0700cfb2efd5e3d41c74ba8e7b4380758a607
Pratham Mishra <mishrapratham119@gmail.com>
2024-02-16 20:03:16 +0530

    search bar

--- bookstore/src/main/java/com/bookStore/repository/BookRepository.java
+++ bookstore/src/main/java/com/bookStore/repository/BookRepository.java
@@ -1,11 +1,15 @@
 package com.bookStore.repository;
 
+import com.bookStore.entity.Book;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bookStore.entity.Book;
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book,Integer>  {
+
+    public List<Book> findByNameContainingAndUser(String name, User user);
 
 }
