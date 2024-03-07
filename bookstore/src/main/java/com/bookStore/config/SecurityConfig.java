07297f4384b2097b38e95fb898d61997618ceb67
Pratham Mishra <mishrapratham119@gmail.com>
2024-03-07 20:54:05 +0530

    WebApp completed

--- bookstore/src/main/java/com/bookStore/config/SecurityConfig.java
+++ bookstore/src/main/java/com/bookStore/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
         http
+                .csrf().disable()
                 .authorizeRequests().requestMatchers("/","/signin","/saveUser","/about").permitAll()
                 .requestMatchers("/user/**").authenticated()
                 .requestMatchers("/images/**","/css/**").permitAll()
